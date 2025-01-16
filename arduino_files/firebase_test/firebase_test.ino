3091f3513aded8028e8438e62930e0070e795885
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2025-01-17 01:14:41 +0800

    init firebase arduino file

---
+++ arduino_files/firebase_test/firebase_test.ino
@@ -0,0 +1,86 @@
+#include <Arduino.h>
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
+
+#include <Firebase_ESP_Client.h>
+
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "MIKRO"
+#define WIFI_PASSWORD "1DEAlist"
+
+/* 2. Define the RTDB URL */
+#define DATABASE_URL "pendeteksi01.firebaseio.com"  //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 3. Define the Firebase Data object */
+FirebaseData fbdo;
+
+/* 4, Define the FirebaseAuth data for authentication data */
+FirebaseAuth auth;
+
+/* Define the FirebaseConfig data for config data */
+FirebaseConfig config;
+
+unsigned long dataMillis = 0;
+int count = 0;
+
+
+void setup() {
+
+  Serial.begin(115200);
+
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+
+  Serial.print("Connecting to Wi-Fi");
+  unsigned long ms = millis();
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+
+  /* Assign the database URL(required) */
+  config.database_url = DATABASE_URL;
+
+  config.signer.test_mode = true;
+
+  // The WiFi credentials are required for Pico W
+  // due to it does not have reconnect feature.
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+  config.wifi.clearAP();
+  config.wifi.addAP(WIFI_SSID, WIFI_PASSWORD);
+#endif
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  Firebase.reconnectNetwork(true);
+
+  // Since v4.4.x, BearSSL engine was used, the SSL buffer need to be set.
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+
+  /* Initialize the library with the Firebase authen and config */
+  Firebase.begin(&config, &auth);
+
+  // Or use legacy authenticate method
+  // Firebase.begin(DATABASE_URL, DATABASE_SECRET);
+}
+
+void loop() {
+  if (millis() - dataMillis > 5000) {
+    dataMillis = millis();
+    Serial.printf("Set int... %s\n", Firebase.RTDB.setInt(&fbdo, "/test/int", count++) ? "ok" : fbdo.errorReason().c_str());
+  }
+}
